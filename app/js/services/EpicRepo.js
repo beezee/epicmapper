0e64d7a4bd499c9e08191f0e148b81283246cb65
Brian Zeligson <bzeligson@localytics.com>
2014-08-09 17:06:27 -0400

    EpicRepo initializes and returns epics spanning specified date

--- app/js/services/EpicRepo.js
+++ app/js/services/EpicRepo.js
@@ -1,11 +1,29 @@
 'use strict';
 
 angular.module('epicMapper.services')
-  .service('EpicRepo', [function() {
+  .service('EpicRepo', ['Epic', function(Epic) {
     var epicRepo = function() {};
 
+    epicRepo.prototype.assignedEpics = [];
+
+    epicRepo.prototype.initializeFromEvents = function(events) {
+      var repo = new epicRepo();
+      repo.assignedEpics = _.chain(events)
+        .map(function(e) { return Epic.withSettings(e); })
+        .filter(function(e) { return e.constructor == Epic && e.isValid(); }).value();
+      return repo;
+    };
+
     epicRepo.prototype.epics = function() {
-      return ['a', 'b'];
+      return this.assignedEpics;
+    };
+
+    epicRepo.prototype.epicsSpanningDate = function(date) {
+      var targetDate = moment(date);
+      return _.filter(this.epics(), function(e) {
+        return moment(e.settings.start).startOf('day') <= targetDate.startOf('day')
+          && targetDate.startOf('day') <= moment(e.settings.end).endOf('day');
+      });
     };
     
     return new epicRepo();
--- test/unit/EpicRepoSpec.js
+++ test/unit/EpicRepoSpec.js
@@ -5,11 +5,45 @@
 describe('EpicRepo', function() {
   beforeEach(module('epicMapper.services'));
 
-
-  describe('epics()', function() {
-    it('should return an array of epics', inject(function(EpicRepo) {
-      expect(EpicRepo.epics()).toEqual(['a', 'b']);
+  describe('initializeFromEvents()', function() {
+    
+    it('takes an array of event hashes and populates repo epics for all valid hashes', 
+        inject(function(EpicRepo, Epic) {
+      var events = [{title: 'invalid'}, 
+                {title: 't', start: '2014-01-01', end: '2014-01-03', cost: 20}];
+      var repo = EpicRepo.initializeFromEvents(events);
+      expect(repo.epics().length).toEqual(1);
+      expect(repo.epics()[0].settings).toEqual(events[1]);
     }));
+  });
+
+  describe('epicsSpanningDate(date)', function() {
+    var events, repo = null;
+
+    beforeEach(inject(function(EpicRepo) {
+      events = [{title: '1', start: '2014-01-01', end: '2014-01-03', cost: 20},
+                {title: '2', start: '2014-01-02', end: '2014-01-04', cost: 20},
+                {title: '3', start: '2014-01-03', end: '2014-01-05', cost: 20}];
+      repo = EpicRepo.initializeFromEvents(events);
+    }));
+
+    it('accepts either date or string', function() {
+      var epics = repo.epicsSpanningDate('2014-01-01');
+      expect(epics.length).toEqual(1);
+      expect(epics[0].settings).toEqual(events[0]);
+      epics = repo.epicsSpanningDate(moment('2014-01-05').toDate());
+      expect(epics.length).toEqual(1);
+      expect(epics[0].settings).toEqual(events[2]);
+    });
+
+    it('returns the correct epics', function() {
+      var epicSettings = function(e) { return e.settings; };
+      var epics = repo.epicsSpanningDate('2014-01-02');
+      expect(_.map(epics, epicSettings)).toEqual(events.slice(0, 2));
+      var epics = repo.epicsSpanningDate('2014-01-04');
+      expect(_.map(epics, epicSettings)).toEqual(events.slice(1, 3));
+    });
+
   });
 });
 
