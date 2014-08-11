35c84438eb29468859661bc7163e26b079526644
Brian Zeligson <bzeligson@localytics.com>
2014-08-10 23:42:23 -0400

    no more dummy events

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -14,13 +14,7 @@ angular.module('epicMapper.controllers', [])
       return _.contains([6, 7], moment(date).isoWeekday());
     };
 
-    $scope.dummyEvents = [
-      {title: '1', start: '2014-08-01', end: '2014-08-03', cost: 20},
-      {title: '2', start: '2014-08-05', end: '2014-08-12', cost: 30},
-      {title: '3', start: '2014-08-04', end: '2014-08-07', cost: 10}
-    ];
-
-    $scope.state = $base64.encode(angular.toJson($scope.dummyEvents));
+    $scope.state = $base64.encode(angular.toJson('{}'));
     ngUrlBind($scope, 'state');
 
     var epicSettings = function() {
